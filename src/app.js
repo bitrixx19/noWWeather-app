151920c85bdf7786012f92cb3400bb3a3cce405d
bitrixx19 <69812848+bitrixx19@users.noreply.github.com>
2020-11-13 12:55:24 +0100

    new update function and css

--- src/app.js
+++ src/app.js
@@ -124,4 +124,17 @@ function formatDate(timestamp) {
   let celsiusLink = document.querySelector("#celsius-link");
   celsiusLink.addEventListener("click", displayCelsiusTemperature);
   
-  search("New York");
+  search("London");
+  // change info to current location
+function searchLocation(position) {
+  let apiKey = "768d5798d6d791324557e57ef0abc21e";
+  let apiUrl = `https://api.openweathermap.org/data/2.5/weather?lat=${position.coords.latitude}&lon=${position.coords.longitude}&appid=${apiKey}&units=metric`;
+  axios.get(apiUrl).then(displayWeatherCondition);
+}
+function getCurrentLocation(event) {
+  event.preventDefault();
+  navigator.geolocation.getCurrentPosition(searchLocation);
+}
+
+let currentLocationButton = document.querySelector("#current-location-button");
+currentLocationButton.addEventListener("click", getCurrentLocation);
